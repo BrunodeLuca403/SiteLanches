de1e57e58211bd0bc217756cc8307e275271d2f8
Bruno de Luca <brunodeluca403@gmail.com>
2022-08-21 07:35:20 -0300

    Adicionando carrinho de compras

---
+++ LanchesMac/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,30 @@
+﻿using LanchesMac.Models;
+using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var itens =_carrinhoCompra.GetCarrinhoCompraItens();
+            _carrinhoCompra.CarrinhoCompraItems = itens;
+
+            var carrinhoCompraVm = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoTotal()
+            };
+
+            return View(carrinhoCompraVm);
+        }
+    }
+}
---
+++ LanchesMac/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using LanchesMac.Models;
+
+namespace LanchesMac.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Lanche> LanchesPreferidos { get; set; }
+
+
+    }
+}
