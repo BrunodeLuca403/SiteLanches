e1f43ba093bd51b78f98b7c713b5b6a5c2dc4f2d
Bruno de Luca <brunodeluca403@gmail.com>
2022-08-25 18:49:17 -0300

    Atualizando Projeto

---
+++ LanchesMac/Controllers/AccountController.cs
@@ -0,0 +1,78 @@
+﻿using LanchesMac.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesMac.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signManager)
+        {
+            _userManager = userManager;
+            _signManager = signManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel loginVM)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(loginVM);
+            }
+
+            var user = await _userManager.FindByNameAsync(loginVM.UserName);
+
+            if (user != null){
+
+                var result = await _signManager.PasswordSignInAsync(user, loginVM.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return View(loginVM.ReturnUrl);
+            }
+            ModelState.AddModelError("", "Falha ao realizar o login");
+            return View(loginVM);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(LoginViewModel registroVM)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(registroVM.UserName);
+                var result = await _userManager.CreateAsync(user, registroVM.Password);
+
+                if(result.Succeeded)
+                {
+                    await _signManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Login", "Account");
+                }
+                else
+                {
+                    this.ModelState.AddModelError("Registro", "Falha ao registrar o usuário");
+                }
+            }
+            return View(registroVM);
+        }
+    }
+}
---
+++ LanchesMac/ViewModel/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LanchesMac.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "O campo {0} está vazio, Favor Informe o seu Usuário")]
+        [Display(Name ="Usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} está vazio, Favor Informe a sua senha")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Senha")]
+        public string Password { get; set; }
+        public string ReturnUrl { get; set; }
+    }
+}
